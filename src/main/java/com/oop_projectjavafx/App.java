d3a238c8c438e25fd7d918f2f48994c5e3ff12d5
Novotny_Filip <novof107@hsd.k12.or.us>
2025-04-09 10:11:36 -0700

    final adjustments

--- src/main/java/com/oop_projectjavafx/App.java
+++ src/main/java/com/oop_projectjavafx/App.java
@@ -21,6 +21,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        Button play = new Button("play");
+        play.setStyle("-fx-background-color: #00ff00");
+
         scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/oop_projectjavafx/PrimaryController.java
+++ src/main/java/com/oop_projectjavafx/PrimaryController.java
@@ -10,18 +10,10 @@ import javafx.scene.control.TextField;
 public class PrimaryController {
 
     @FXML
+    Button play = new Button("play");
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-
-        Text title = new Text("Title");
-        Button play = new Button("Play");
-
-        TextField name = new TextField();
-
-        GridPane gridPane = new GridPane();
-
-        gridPane.add(title, 0, 0);
-        gridPane.add(play, 0, 2);
-        gridPane.add(name, 0, 0);
     }
 }
